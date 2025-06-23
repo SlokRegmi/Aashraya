4543e2f7a7b3d7cabc77bb262c57288f0bb22d2f
Slok Regmi <slokr86@gmail.com>
2025-06-23 08:28:37 +0545

    Project Structure setup for the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,13 @@
-import express, { Request, Response } from "express"
-import morgan from "morgan"
-import { CorsMiddleware } from "./middleware/cors.middleware"
-const app=express()
+import express from 'express';
 
+const app = express();
 
-app.use(morgan("dev"))
-app.use(CorsMiddleware)
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+// Middlewares
+app.use(express.json()); // For JSON parsing
 
-app.use("/",(req:Request,res:Response)=>{
-    res.send("<>Hello world</>")
-})
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello from the Express + TypeScript server!');
+});
 
 export default app;
